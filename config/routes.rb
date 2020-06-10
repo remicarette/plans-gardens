a3d9cb15912574f6721b61e4e9fd73353a67ecd9
remi carette <carette.remi@gmail.com>
2020-06-10 17:52:42 +0200

    routes are ready to go

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :gardens, only: [:show, :create]
+  resources :plants, only: [:destroy]
 end
